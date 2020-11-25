a36d256f8d82647e836cc84ccfc2d9a05189570e
23josecin <74673559+23josecin@users.noreply.github.com>
2020-11-25 14:19:40 -0500

    Create cole.js

---
+++ cole.js
@@ -0,0 +1,17 @@
+function cli(){
+var but=document.getElementById("btn").style.color="red"}
+function clo(){var but=document.getElementById("btn").style.color="#000"}
+function hvu(){
+var but=document.getElementById("btn").style;
+but.backgroundColor="#00A000";}
+function hvo(){
+var but=document.getElementById("btn").style;
+but.backgroundColor="#00ff00";}
+function bt(){
+var but=document.getElementById("btn").style;
+but.color="#000";
+but.borderColor="#000";
+but.backgroundColor="#00ff00";
+but.cursor="pointer";}
+function red(){
+var x=document.getElementByName("user-agent-custom-assign-slot").style.backgroundColor="red";}
